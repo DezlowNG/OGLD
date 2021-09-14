328e6ae1b1dd22585701a77a43cd89dc7fcc3cbe
DezlowNG <dezlow@bk.ru>
2021-09-14 15:22:22 +0700

    --

--- OGLD/Source/Core/Core.cpp
+++ OGLD/Source/Core/Core.cpp
@@ -17,12 +17,12 @@ unsigned int loadTexture(const char* texturePath)
 		if (nrComponents == 1)
 			format = GL_RED;
 		else if (nrComponents == 3)
-			format = GL_SRGB;
+			format = GL_RGB;
 		else if (nrComponents == 4)
 			format = GL_RGBA;
 
 		glBindTexture(GL_TEXTURE_2D, ID);
-		glTexImage2D(GL_TEXTURE_2D, 0, format, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, imageWidth, imageHeight, 0, format, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
