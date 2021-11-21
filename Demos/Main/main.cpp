55607bf9c0fec243e9e55a718895183de6bb3147
DezlowNG <dezlow@bk.ru>
2021-11-21 16:28:11 +0300

    new source tree

---
+++ Demos/Main/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by dezlow on 21.11.2021.
+//
+
+#include "library.h"
+
+int main()
+{
+    hello();
+    return 0;
+}
---
+++ OGLD/Source/library.h
@@ -0,0 +1,6 @@
+#ifndef OGLD_LIBRARY_H
+#define OGLD_LIBRARY_H
+
+void hello();
+
+#endif //OGLD_LIBRARY_H
